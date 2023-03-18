8ed672cce72d62427d9923984f5d5f765fd7b148
Chenzengrui <1281140948@qq.com>
2023-03-18 16:32:47 +0800

    modify1

--- data preprocessing_Matlab/download_MITBIHDB.m
+++ data preprocessing_Matlab/download_MITBIHDB.m
@@ -12,11 +12,11 @@
 record_list = physionetdb('mitdb');
 
 
-path_to_exes = 'path_to_WFDB_Toolbox\mcode\nativelibs\windows\bin';
+path_to_exes = 'path_to_WFDB_Toolbox/mcode/nativelibs/windows/bin';
 path_to_save_records = 'path_to_downloaded_database';
 
-% path_to_exes = 'C:\my_files\ECG_research\mcode\nativelibs\windows\bin';
-% path_to_save_records = 'C:\my_files\ECG_dataset\MIT-BIH\mitbihdb';
+% path_to_exes = 'C:/my_files/ECG_research/mcode/nativelibs/windows/bin';
+% path_to_save_records = 'C:/my_files/ECG_dataset/MIT-BIH/mitbihdb';
 
 mkdir(path_to_save_records);
 cd(path_to_save_records);
@@ -28,8 +28,8 @@ for i=1:length(record_list)
     command_mat_info_ = char(strcat(path_to_exes, filesep, 'wfdb2mat.exe -r mitdb/', record_list(i), ' >', record_list(i), 'm.info'));
     system (command_mat_info_);
     
-%     system('C:\my_files\ECG_research\mcode\nativelibs\windows\bin\wfdb2mat.exe -r 100s -f 0 -t 10 >100sm.info')
-%     system('C:\my_files\ECG_research\mcode\nativelibs\windows\bin\rdann.exe -r mitdb/100 -a atr >100.txt')
+%     system('C:/my_files/ECG_research/mcode/nativelibs/windows/bin/wfdb2mat.exe -r 100s -f 0 -t 10 >100sm.info')
+%     system('C:/my_files/ECG_research/mcode/nativelibs/windows/bin/rdann.exe -r mitdb/100 -a atr >100.txt')
     
 end
 toc
--- seq_seq_annot_DS1DS2.py
+++ seq_seq_annot_DS1DS2.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.io as spio
-from  sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import MinMaxScaler
 import random
 import time
 import os
@@ -13,6 +13,8 @@ from sklearn.model_selection import train_test_split
 import argparse
 
 random.seed(654)
+
+
 def read_mitbih(filename, max_time=100, classes= ['F', 'N', 'S', 'V', 'Q'], max_nlabel=100, trainset=1):
     def normalize(data):
         data = np.nan_to_num(data)  # removing NaNs and Infs
@@ -23,10 +25,10 @@ def read_mitbih(filename, max_time=100, classes= ['F', 'N', 'S', 'V', 'Q'], max_
     # read data
     data = []
     samples = spio.loadmat(filename + ".mat")
-    if trainset == 1: #DS1
+    if trainset == 1:  # DS1
         samples = samples['s2s_mitbih_DS1']
 
-    else: # DS2
+    else:  # DS2
         samples = samples['s2s_mitbih_DS2']
 
     values = samples[0]['seg_values']
@@ -46,12 +48,12 @@ def read_mitbih(filename, max_time=100, classes= ['F', 'N', 'S', 'V', 'Q'], max_
             l_data = l_data + 1
 
     #  add all labels together
-    l_lables  = 0
+    l_lables = 0
     t_lables = []
     for i, item in enumerate(labels):
-        if len(t_lables)==n_seqs*max_time:
+        if len(t_lables) == n_seqs * max_time:
             break
-        item= item[0]
+        item = item[0]
         for lebel in item:
             if l_lables == n_seqs * max_time:
                 break
@@ -63,8 +65,8 @@ def read_mitbih(filename, max_time=100, classes= ['F', 'N', 'S', 'V', 'Q'], max_
     shape_v = data.shape
     data = np.reshape(data, [shape_v[0], -1])
     t_lables = np.array(t_lables)
-    _data  = np.asarray([],dtype=np.float64).reshape(0,shape_v[1])
-    _labels = np.asarray([],dtype=np.dtype('|S1')).reshape(0,)
+    _data = np.asarray([], dtype=np.float64).reshape(0, shape_v[1])
+    _labels = np.asarray([], dtype=np.dtype('|S1')).reshape(0, )
     for cl in classes:
         _label = np.where(t_lables == cl)
         permute = np.random.permutation(len(_label[0]))
@@ -75,7 +77,7 @@ def read_mitbih(filename, max_time=100, classes= ['F', 'N', 'S', 'V', 'Q'], max_
         _data = np.concatenate((_data, data[_label]))
         _labels = np.concatenate((_labels, t_lables[_label]))
 
-    data = _data[:(len(_data)/ max_time) * max_time, :]
+    data = _data[:(len(_data) / max_time) * max_time, :]
     _labels = _labels[:(len(_data) / max_time) * max_time]
 
     # data = _data
@@ -86,12 +88,14 @@ def read_mitbih(filename, max_time=100, classes= ['F', 'N', 'S', 'V', 'Q'], max_
     permute = np.random.permutation(len(labels))
     data = np.asarray(data)
     labels = np.asarray(labels)
-    data= data[permute]
+    data = data[permute]
     labels = labels[permute]
 
     print('Records processed!')
 
     return data, labels
+
+
 def evaluate_metrics(confusion_matrix):
     # https://stackoverflow.com/questions/31324218/scikit-learn-how-to-obtain-true-positive-true-negative-false-positive-and-fal
     # Sensitivity, hit rate, recall, or true positive rate
@@ -120,16 +124,21 @@ def evaluate_metrics(confusion_matrix):
         ACC)  # to get a sense of effectiveness of our method on the small classes we computed this average (macro-average)
 
     return ACC_macro, ACC, TPR, TNR, PPV
+
+
 def batch_data(x, y, batch_size):
     shuffle = np.random.permutation(len(x))
     start = 0
-#     from IPython.core.debugger import Tracer; Tracer()()
+    #     from IPython.core.debugger import Tracer; Tracer()()
     x = x[shuffle]
     y = y[shuffle]
     while start + batch_size <= len(x):
-        yield x[start:start+batch_size], y[start:start+batch_size]
+        yield x[start:start + batch_size], y[start:start + batch_size]
         start += batch_size
-def build_network(inputs, dec_inputs,char2numY,n_channels=10,input_depth=280,num_units=128,max_time=10,bidirectional=False):
+
+
+def build_network(inputs, dec_inputs, char2numY, n_channels=10, input_depth=280, num_units=128, max_time=10,
+                  bidirectional=False):
     _inputs = tf.reshape(inputs, [-1, n_channels, input_depth / n_channels])
     # _inputs = tf.reshape(inputs, [-1,input_depth,n_channels])
 
@@ -143,10 +152,10 @@ def build_network(inputs, dec_inputs,char2numY,n_channels=10,input_depth=280,num
     max_pool_2 = tf.layers.max_pooling1d(inputs=conv2, pool_size=2, strides=2, padding='same')
 
     conv3 = tf.layers.conv1d(inputs=max_pool_2, filters=128, kernel_size=2, strides=1,
-                              padding='same', activation=tf.nn.relu)
+                             padding='same', activation=tf.nn.relu)
 
     shape = conv3.get_shape().as_list()
-    data_input_embed = tf.reshape(conv3, (-1, max_time,shape[1]*shape[2]))
+    data_input_embed = tf.reshape(conv3, (-1, max_time, shape[1] * shape[2]))
 
     # timesteps = max_time
     #
@@ -159,12 +168,11 @@ def build_network(inputs, dec_inputs,char2numY,n_channels=10,input_depth=280,num
     # data_input_embed = tf.stack(data_input_embed, 1)
     # shape = data_input_embed.get_shape().as_list()
 
-    embed_size = 10 #128 lstm_size # shape[1]*shape[2]
+    embed_size = 10  # 128 lstm_size # shape[1]*shape[2]
 
     # Embedding layers
     output_embedding = tf.Variable(tf.random_uniform((len(char2numY), embed_size), -1.0, 1.0), name='dec_embedding')
     data_output_embed = tf.nn.embedding_lookup(output_embedding, dec_inputs)
-
 
     with tf.variable_scope("encoding") as encoding_scope:
         if not bidirectional:
@@ -199,6 +207,8 @@ def build_network(inputs, dec_inputs,char2numY,n_channels=10,input_depth=280,num
     logits = tf.layers.dense(dec_outputs, units=len(char2numY), use_bias=True)
 
     return logits
+
+
 def str2bool(v):
     if v.lower() in ('yes', 'true', 't', 'y', '1'):
         return True
@@ -206,29 +216,55 @@ def str2bool(v):
         return False
     else:
         raise argparse.ArgumentTypeError('Boolean value expected.')
+
+
 def main():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--epochs', type=int, default=500)
-    parser.add_argument('--max_time', type=int, default=10)
-    parser.add_argument('--test_steps', type=int, default=10)
-    parser.add_argument('--batch_size', type=int, default=20)
-    parser.add_argument('--data_dir', type=str, default='data/s2s_mitbih_aami_DS1DS2')
-    parser.add_argument('--bidirectional', type=str2bool, default=str2bool('False'))
+    parser.add_argument('--epochs',
+                        type=int,
+                        default=500)
+    parser.add_argument('--max_time',
+                        type=int,
+                        default=10)
+    parser.add_argument('--test_steps',
+                        type=int,
+                        default=10)
+    parser.add_argument('--batch_size',
+                        type=int,
+                        default=20)
+    parser.add_argument('--data_dir',
+                        type=str,
+                        default='data/s2s_mitbih_aami_DS1DS2')
+    parser.add_argument('--bidirectional',
+                        type=str2bool,
+                        default=str2bool('False'))
     # parser.add_argument('--lstm_layers', type=int, default=2)
-    parser.add_argument('--num_units', type=int, default=128)
-    parser.add_argument('--n_oversampling', type=int, default=6000)
-    parser.add_argument('--checkpoint_dir', type=str, default='checkpoints-seq2seq_DS1DS2')
-    parser.add_argument('--ckpt_name', type=str, default='seq2seq_mitbih_DS1DS2.ckpt')
-    parser.add_argument('--classes', nargs='+', type=chr,
-                        default=['N', 'S','V'])
+    parser.add_argument('--num_units',
+                        type=int,
+                        default=128)
+    parser.add_argument('--n_oversampling',
+                        type=int,
+                        default=6000)
+    parser.add_argument('--checkpoint_dir',
+                        type=str,
+                        default='checkpoints-seq2seq_DS1DS2')
+    parser.add_argument('--ckpt_name',
+                        type=str,
+                        default='seq2seq_mitbih_DS1DS2.ckpt')
+    parser.add_argument('--classes',
+                        nargs='+',
+                        type=chr,
+                        default=['N', 'S', 'V'])
     args = parser.parse_args()
     run_program(args)
+
+
 def run_program(args):
     print(args)
-    max_time = args.max_time # 5 3 second best 10# 40 # 100
-    epochs = args.epochs # 300
-    batch_size = args.batch_size # 10
+    max_time = args.max_time  # 5 3 second best 10# 40 # 100
+    epochs = args.epochs  # 300
+    batch_size = args.batch_size  # 10
     num_units = args.num_units
     bidirectional = args.bidirectional
     # lstm_layers = args.lstm_layers
@@ -236,28 +272,27 @@ def run_program(args):
     checkpoint_dir = args.checkpoint_dir
     ckpt_name = args.ckpt_name
     test_steps = args.test_steps
-    classes= args.classes # ['N', 'S','V']
+    classes = args.classes  # ['N', 'S','V']
     filename = args.data_dir
 
-    X_train, y_train = read_mitbih(filename, max_time, classes=classes, max_nlabel=50000,trainset=1)
-    X_test, y_test = read_mitbih(filename, max_time, classes=classes, max_nlabel=50000,trainset=0)
+    X_train, y_train = read_mitbih(filename, max_time, classes=classes, max_nlabel=50000, trainset=1)
+    X_test, y_test = read_mitbih(filename, max_time, classes=classes, max_nlabel=50000, trainset=0)
     input_depth = X_train.shape[2]
     n_channels = 10
-    print ("# of sequences: ", len(X_train))
+    print("# of sequences: ", len(X_train))
 
     classes = np.unique(y_train)
     char2numY = dict(zip(classes, range(len(classes))))
     n_classes = len(classes)
-    print ('Classes (training): ', classes)
+    print('Classes (training): ', classes)
     for cl in classes:
         ind = np.where(classes == cl)[0][0]
-        print (cl, len(np.where(y_train.flatten() == cl)[0]))
+        print(cl, len(np.where(y_train.flatten() == cl)[0]))
 
-    print ('Classes (test): ', classes)
+    print('Classes (test): ', classes)
     for cl in classes:
         ind = np.where(classes == cl)[0][0]
-        print (cl, len(np.where(y_test.flatten() == cl)[0]))
-
+        print(cl, len(np.where(y_test.flatten() == cl)[0]))
 
     char2numY['<GO>'] = len(char2numY)
     num2charY = dict(zip(char2numY.values(), char2numY.keys()))
@@ -275,8 +310,9 @@ def run_program(args):
     targets = tf.placeholder(tf.int32, (None, None), 'targets')
     dec_inputs = tf.placeholder(tf.int32, (None, None), 'output')
 
-    logits = build_network(inputs, dec_inputs, char2numY, n_channels=n_channels, input_depth=input_depth, num_units=num_units, max_time=max_time,
-                  bidirectional=bidirectional)
+    logits = build_network(inputs, dec_inputs, char2numY, n_channels=n_channels, input_depth=input_depth,
+                           num_units=num_units, max_time=max_time,
+                           bidirectional=bidirectional)
     # decoder_prediction = tf.argmax(logits, 2)
     # confusion = tf.confusion_matrix(labels=tf.argmax(targets, 1), predictions=tf.argmax(logits, 2), num_classes=len(char2numY) - 1)# it is wrong
     # mean_accuracy,update_mean_accuracy = tf.metrics.mean_per_class_accuracy(labels=targets, predictions=decoder_prediction, num_classes=len(char2numY) - 1)
@@ -286,54 +322,53 @@ def run_program(args):
         vars = tf.trainable_variables()
         beta = 0.001
         lossL2 = tf.add_n([tf.nn.l2_loss(v) for v in vars
-                            if 'bias' not in v.name]) * beta
+                           if 'bias' not in v.name]) * beta
         loss = tf.contrib.seq2seq.sequence_loss(logits, targets, tf.ones([batch_size, y_seq_length]))
         # Optimizer
         loss = tf.reduce_mean(loss + lossL2)
         optimizer = tf.train.RMSPropOptimizer(1e-3).minimize(loss)
 
-
     # train the graph
 
     # over-sampling: SMOTE
-    X_train = np.reshape(X_train,[X_train.shape[0]*X_train.shape[1],-1])
-    y_train= y_train[:,1:].flatten()
+    X_train = np.reshape(X_train, [X_train.shape[0] * X_train.shape[1], -1])
+    y_train = y_train[:, 1:].flatten()
 
     nums = []
     for cl in classes:
         ind = np.where(classes == cl)[0][0]
-        nums.append(len(np.where(y_train.flatten()==ind)[0]))
+        nums.append(len(np.where(y_train.flatten() == ind)[0]))
 
     # ratio={0:nums[0],1:nums[0],2:nums[0]}
     # ratio={0:7000,1:nums[1],2:7000,3:7000}
-    ratio={0:nums[0],1:n_oversampling+1000,2:n_oversampling}
-    sm = SMOTE(random_state=12,ratio=ratio)
+    ratio = {0: nums[0], 1: n_oversampling + 1000, 2: n_oversampling}
+    sm = SMOTE(random_state=12, ratio=ratio)
     X_train, y_train = sm.fit_sample(X_train, y_train)
 
-    X_train = X_train[:(X_train.shape[0]/max_time)*max_time,:]
-    y_train = y_train[:(X_train.shape[0]/max_time)*max_time]
+    X_train = X_train[:(X_train.shape[0] / max_time) * max_time, :]
+    y_train = y_train[:(X_train.shape[0] / max_time) * max_time]
 
-    X_train = np.reshape(X_train,[-1,X_test.shape[1],X_test.shape[2]])
-    y_train = np.reshape(y_train,[-1,y_test.shape[1]-1,])
-    y_train= [[char2numY['<GO>']] + [y_ for y_ in date] for date in y_train]
+    X_train = np.reshape(X_train, [-1, X_test.shape[1], X_test.shape[2]])
+    y_train = np.reshape(y_train, [-1, y_test.shape[1] - 1, ])
+    y_train = [[char2numY['<GO>']] + [y_ for y_ in date] for date in y_train]
     y_train = np.array(y_train)
 
-    print ('Classes in the training set: ', classes)
+    print('Classes in the training set: ', classes)
     for cl in classes:
         ind = np.where(classes == cl)[0][0]
-        print (cl, len(np.where(y_train.flatten()==ind)[0]))
-    print ("------------------y_train samples--------------------")
+        print(cl, len(np.where(y_train.flatten() == ind)[0]))
+    print("------------------y_train samples--------------------")
     for ii in range(2):
-      print(''.join([num2charY[y_] for y_ in list(y_train[ii+5])]))
+        print(''.join([num2charY[y_] for y_ in list(y_train[ii + 5])]))
 
-    print ('Classes in the training set: ', classes)
+    print('Classes in the training set: ', classes)
     for cl in classes:
         ind = np.where(classes == cl)[0][0]
-        print (cl, len(np.where(y_test.flatten()==ind)[0]))
+        print(cl, len(np.where(y_test.flatten() == ind)[0]))
 
-    print ("------------------y_test samples--------------------")
+    print("------------------y_test samples--------------------")
     for ii in range(2):
-      print(''.join([num2charY[y_] for y_ in list(y_test[ii+5])]))
+        print(''.join([num2charY[y_] for y_ in list(y_test[ii + 5])]))
 
     def test_model():
         # source_batch, target_batch = next(batch_data(X_test, y_test, batch_size))
@@ -349,11 +384,11 @@ def run_program(args):
                 dec_input = np.hstack([dec_input, prediction[:, None]])
             # acc_track.append(np.mean(dec_input == target_batch))
             acc_track.append(dec_input[:, 1:] == target_batch[:, 1:])
-            y_true= target_batch[:, 1:].flatten()
+            y_true = target_batch[:, 1:].flatten()
             y_pred = dec_input[:, 1:].flatten()
-            sum_test_conf.append(confusion_matrix(y_true, y_pred,labels=range(len(char2numY)-1)))
+            sum_test_conf.append(confusion_matrix(y_true, y_pred, labels=range(len(char2numY) - 1)))
 
-        sum_test_conf= np.mean(np.array(sum_test_conf, dtype=np.float32), axis=0)
+        sum_test_conf = np.mean(np.array(sum_test_conf, dtype=np.float32), axis=0)
 
         # print('Accuracy on test set is: {:>6.4f}'.format(np.mean(acc_track)))
 
@@ -366,19 +401,23 @@ def run_program(args):
         acc_avg, acc, sensitivity, specificity, PPV = evaluate_metrics(sum_test_conf)
         print('Average Accuracy is: {:>6.4f} on test set'.format(acc_avg))
         for index_ in range(n_classes):
-            print("\t{} rhythm -> Sensitivity: {:1.4f}, Specificity : {:1.4f}, Precision (PPV) : {:1.4f}, Accuracy : {:1.4f}".format(
-                classes[index_],
-                sensitivity[
-                    index_],
-                specificity[
-                    index_], PPV[index_],
-                acc[index_]))
-        print("\t Average -> Sensitivity: {:1.4f}, Specificity : {:1.4f}, Precision (PPV) : {:1.4f}, Accuracy : {:1.4f}".format(
-            np.mean(sensitivity), np.mean(specificity), np.mean(PPV), np.mean(acc)))
-        return  acc_avg, acc, sensitivity, specificity, PPV
+            print(
+                "\t{} rhythm -> Sensitivity: {:1.4f}, Specificity : {:1.4f}, Precision (PPV) : {:1.4f}, Accuracy : {:1.4f}".format(
+                    classes[index_],
+                    sensitivity[
+                        index_],
+                    specificity[
+                        index_], PPV[index_],
+                    acc[index_]))
+        print(
+            "\t Average -> Sensitivity: {:1.4f}, Specificity : {:1.4f}, Precision (PPV) : {:1.4f}, Accuracy : {:1.4f}".format(
+                np.mean(sensitivity), np.mean(specificity), np.mean(PPV), np.mean(acc)))
+        return acc_avg, acc, sensitivity, specificity, PPV
+
     loss_track = []
+
     def count_prameters():
-        print ('# of Params: ', np.sum([np.prod(v.get_shape().as_list()) for v in tf.trainable_variables()]))
+        print('# of Params: ', np.sum([np.prod(v.get_shape().as_list()) for v in tf.trainable_variables()]))
 
     count_prameters()
     if (os.path.exists(checkpoint_dir) == False):
@@ -407,18 +446,20 @@ def run_program(args):
                 train_acc = []
                 for batch_i, (source_batch, target_batch) in enumerate(batch_data(X_train, y_train, batch_size)):
                     _, batch_loss, batch_logits = sess.run([optimizer, loss, logits],
-                        feed_dict = {inputs: source_batch,
-                                     dec_inputs: target_batch[:, :-1],
-                                     targets: target_batch[:, 1:]})
+                                                           feed_dict={inputs: source_batch,
+                                                                      dec_inputs: target_batch[:, :-1],
+                                                                      targets: target_batch[:, 1:]})
                     loss_track.append(batch_loss)
-                    train_acc.append(batch_logits.argmax(axis=-1) == target_batch[:,1:])
+                    train_acc.append(batch_logits.argmax(axis=-1) == target_batch[:, 1:])
 
                 accuracy = np.mean(train_acc)
-                print('Epoch {:3} Loss: {:>6.3f} Accuracy: {:>6.4f} Epoch duration: {:>6.3f}s'.format(epoch_i, batch_loss,
-                                                                                  accuracy, time.time() - start_time))
+                print(
+                    'Epoch {:3} Loss: {:>6.3f} Accuracy: {:>6.4f} Epoch duration: {:>6.3f}s'.format(epoch_i, batch_loss,
+                                                                                                    accuracy,
+                                                                                                    time.time() - start_time))
 
-                if epoch_i%test_steps==0:
-                    acc_avg, acc, sensitivity, specificity, PPV= test_model()
+                if epoch_i % test_steps == 0:
+                    acc_avg, acc, sensitivity, specificity, PPV = test_model()
 
                     print('loss {:.4f} after {} epochs (batch_size={})'.format(loss_track[-1], epoch_i + 1, batch_size))
                     save_path = os.path.join(checkpoint_dir, ckpt_name)
@@ -437,9 +478,7 @@ def run_program(args):
         print(str(datetime.now()))
 
         # test_model()
+
+
 if __name__ == '__main__':
     main()
-
-
-
-
